33fe9754f1ab7ad844d304dbe0fe487fff412ef6
kasunijayasekara <kasunikoshila@gmail.com>
2025-09-06 11:59:20 +0530

    Add About page component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
-
+import About from "./pages/About";
 import Footer from "./components/Footer";
 
 
@@ -15,6 +15,7 @@ function App() {
       <div className="pt-0"> {/* padding so navbar won't overlap */}
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
           
         </Routes>
       </div>
