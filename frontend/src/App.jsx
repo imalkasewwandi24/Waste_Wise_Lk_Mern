0315a461bb0383a2e9bbd4b91544f7c6f39aed1c
kasunijayasekara <kasunikoshila@gmail.com>
2025-09-06 16:20:21 +0530

    Add Contact page component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
-
 import Footer from "./components/Footer";
+import Contact from "./pages/Contact";
 
 
 
@@ -15,6 +15,7 @@ function App() {
       <div className="pt-0"> {/* padding so navbar won't overlap */}
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<Contact/>}/>
           
         </Routes>
       </div>
